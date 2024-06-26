18638e71edf2e8474607de9800dc928fe6962899
AbhijithKumble <abhijithkumble87564@gmail.com>
2024-06-26 16:29:27 +0530

    added tailwind, postcss, autofixer, @types/node and also a script to check if the package manager used is pnpm

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css'
 
 const App = () => {
-    return (
-        <>
-            <h1>Welcome to EduShare</h1>
-        </>
-    );
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 }
 
 export default App
