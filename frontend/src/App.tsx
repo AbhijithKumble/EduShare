5f22a203a93e3448add6180ce302a05eec147464
AbhijithKumble <abhijithkumble87564@gmail.com>
2024-06-27 22:20:44 +0530

    configured tailwind and vite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
-import './App.css'
-
 const App = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
 
-export default App
+  return (
+    <>
+      <h1 className="text-xl text-center text-blue-200">Hello World!</h1>
+    </>
+  );
+};
+
+export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-theme: {
+  theme: {
     extend: {},
   },
   plugins: [],
