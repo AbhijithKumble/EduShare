06e1838e07d1449acd477b747a36a6fc2392e7d6
AbhijithKumble <abhijithkumble87564@gmail.com>
2024-06-28 12:41:36 +0530

    added shadcn-ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,46 +1,27 @@
 const App = () => {
 
+    //    <div className="hidden md:block bg-green-50">
+    //        <div className="bg-blue-50 h-full pt-20">
+    //            <img alt="logo" src="/logo.svg" className="h-[165px] w-[120px] m-auto" />
+    //            <h1 className="text-center font-grotesque font-bold text-8xl">Edu Share</h1>
+    //            <h3 className="font-grotesque font-medium text-4xl text-center pt-10">
+    //                Unlock your potential with Shared Knowledge
+    //            </h3>
+    //        </div>
+    //    </div>
 
     return (
-        <div className="bg-black h-screen px-12 py-8">
-            <div className="bg-white h-full rounded-xl grid md:grid-cols-2 p-2">
-                <div className="hidden md:block bg-green-50">
-                    <div className="bg-blue-50 h-full pt-20">
-                        <img alt="logo" src="/logo.svg" className="h-[165px] w-[120px] m-auto" />
-                        <h1 className="text-center font-grotesque font-bold text-8xl">Edu Share</h1>
-                        <h3 className="font-grotesque font-medium text-4xl text-center pt-10">
-                            Unlock your potential with Shared Knowledge
-                        </h3>
-                    </div>
-                </div>
-                <div className="bg-red-50 p-20 flex justify-center">
-                    <div className="bg-green-100 h-full rounded-xl p-2 w-[450px]">
+        <div className="bg-black h-screen sm:px-12 sm:py-8">
+            <div className="bg-white h-full sm:rounded-xl grid md:grid-cols-2">
+                <div className="bg-red-50 flex justify-center">
+                    <div className="bg-green-100 h-full rounded-xl">
                         <h1 className="font-poppins font-medium text-4xl text-center">Login</h1>
+
                     </div>
                 </div>
             </div>
         </div>
     );
-    /*    return (
-            <div className="bg-black h-screen px-12 py-8">
-                <div className="bg-white h-full rounded-xl grid md:grid-cols-2 p-2">
-                    <div className="max-md:hidden bg-green-50 ">
-                        <div className="bg-blue-50 h-full pt-20">
-                            <img alt="logo" src="/logo.svg" className="h-[165px] w-[120px] m-auto " />
-                            <h1 className="text-center font-grotesque font-bold text-8xl">Edu Share</h1>
-                            <h3 className="font-grotesque font-medium text-4xl text-center pt-10">Unlock your potential with Shared Knowledge</h3>
-                        </div>
-                    </div>
-                    <div className="bg-red-50 p-20 flex justify-center">
-                       <div className="bg-green-100 h-full rounded-xl p-2 w-[450px]">
-                            <h1 className="font-poppins font-medium text-4xl text-center">Login</h1>
-                        </div> 
-                    </div>
-                </div>
-            </div>
-        );*/
-
-
 };
 
 export default App;
